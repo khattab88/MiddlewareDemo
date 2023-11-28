1abf97bf56c723f2ff1d364730d903fd7f6196ff
khattab88 <m_khatab_88@hotmail.com>
2023-11-28 15:33:43 +0200

    create first middleware

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -21,20 +21,7 @@ namespace WebApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.Run(async context => await context.Response.WriteAsync("welcome to middleware"));
         }
     }
 }
